0b445458b32a51bacbe99b0b6b822f1a52aaa771
martyx <martin1.bugar@gmail.com>
2022-12-30 14:24:38 +0100

    expenses added to dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -15,6 +15,8 @@
         <ul class="list-group">
             <li class="list-group-item">Spolu za rok 2022 : <strong> {{ sumOfValuesFrom2022() }} € </strong></li>
             <li class="list-group-item">Spolu celkom: <strong> {{ sumOfValues() }} € </strong></li>
+            <li class="list-group-item">Spolu vydaje za rok 2022 : <strong> {{ sumOfExpensesFrom2022() }} € </strong></li>
+            <li class="list-group-item">Spolu vydaje celkom: <strong> {{ sumOfExpenses() }} € </strong></li>
         </ul>
     </div>
 </template>
@@ -35,6 +37,7 @@ export default {
             name: null,
             email: null,
             posts: [],
+            expenses: [],
         }
     },
     created() {
@@ -42,6 +45,16 @@ export default {
             this.$axios.get('/api/posts')
                 .then(response => {
                     this.posts = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        });
+
+        this.$axios.get('/sanctum/csrf-cookie').then(response => {
+            this.$axios.get('/api/expenses')
+                .then(response => {
+                    this.expenses = response.data;
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -67,6 +80,25 @@ export default {
         sumOfValuesFrom2022() {
             let sum = 0;
             this.posts.forEach((value) => {
+                let date = new Date(value.date);
+                if (value.userID === this.userId && date.getFullYear() === 2022) {
+                    sum = sum + value.value;
+                }
+            });
+            return sum;
+        },
+        sumOfExpenses() {
+            let sum = 0;
+            this.expenses.forEach((value) => {
+                if (value.userID === this.userId) {
+                    sum = sum + value.value;
+                }
+            });
+            return sum;
+        },
+        sumOfExpensesFrom2022() {
+            let sum = 0;
+            this.expenses.forEach((value) => {
                 let date = new Date(value.date);
                 if (value.userID === this.userId && date.getFullYear() === 2022) {
                     sum = sum + value.value;
