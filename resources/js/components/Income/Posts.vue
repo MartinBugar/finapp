460e4ce6d940d0f0a21fb6ddcdf043a17cf01de8
martyx <martin1.bugar@gmail.com>
2023-01-03 09:10:35 +0100

    fix som issues with pdf

--- resources/js/components/Income/Posts.vue
+++ resources/js/components/Income/Posts.vue
@@ -58,7 +58,7 @@
                         <td>{{ post.description }}</td>
                         <td>{{ post.value }} €</td>
                         <td class="text-center">
-                            <div v-if="post.pdf" @click="downloadWithAxios(post.pdf)">dokument</div>
+                            <a href="#" v-if="post.pdf" @click="downloadWithAxios(post.pdf)">{{ post.pdf }}</a>
                         </td>
 
                         <td class="text-center buttons" v-if="userID === post.userID">
@@ -124,17 +124,17 @@ export default {
             })
                 .then(response => {
 
-                    this.forceFileDownload(response)
+                    this.forceFileDownload(response, pdf)
 
                 })
                 .catch(() => console.log('error occured'))
         },
 
-        forceFileDownload(response){
+        forceFileDownload(response, pdf){
             const url = window.URL.createObjectURL(new Blob([response.data]))
             const link = document.createElement('a')
             link.href = url
-            link.setAttribute('download', 'file.pdf') //or any other extension
+            link.setAttribute('download', pdf ) //or any other extension
             document.body.appendChild(link)
             link.click()
         },
