e4936f25bd4fed18f44319cf402648b688e96632
finapp <martin1.bugar@gmail.com>
2023-02-16 20:11:40 +0100

    installing bootstrap vue 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,10 @@ import {createApp} from 'vue';
 
 require('./bootstrap');
 import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
+import BootstrapVue3 from 'bootstrap-vue-3'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 import App from './App.vue';
 import axios from 'axios';
@@ -13,5 +16,6 @@ import router from './router';
 const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.use(BootstrapIconsPlugin);
+app.use(BootstrapVue3);
 app.use(router);
 app.mount('#app');
