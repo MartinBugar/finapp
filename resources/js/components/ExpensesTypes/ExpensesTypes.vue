5d2a84bde9af2ad366ee4f4e4b15f584a29f124d
finapp <martin1.bugar@gmail.com>
2023-02-19 09:08:37 +0100

    responsivity off expenses types

--- resources/js/components/ExpensesTypes/ExpensesTypes.vue
+++ resources/js/components/ExpensesTypes/ExpensesTypes.vue
@@ -1,57 +1,51 @@
 <template>
     <div class="bg-expenses-types">
-        <div class="container aaa">
-            <div class="row jutify-content-center">
-                <div class="col-md-7" style="float:none; margin:auto;">
-                    <div class="card cardExpensesTypes">
-                        <div class="card-body">
-                            <div class="d-flex justify-content-between pb-2 mb-2">
-                                <h3 class="card-title"><strong>Typy transakcií</strong>
-                                </h3>
-                                <div>
-                                    <button class="btn btn-success buttonNewExpense" type="button"
-                                            @click="this.$router.push('/expensestypes/add')">
-                                        Vytvoriť nový typ transakcie
-                                    </button>
-                                </div>
-                            </div>
-
-                            <table class="table table-hover table-sm table-bordered table-dark">
-                                <thead class="bg-dark text-light">
-                                <tr>
-                                    <th width="50" class="text-center">#</th>
-                                    <!--                                <th width="100" class="text-center">id</th>-->
-                                    <!--                                <th width="100" class="text-center">userId</th>-->
-                                    <th class="text-center">Typ transakcie</th>
-                                    <th class="text-center" width="200">Actions</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr class=""
-                                    v-for="(expensType, index) in filteredAndSortedExpensesTypes(expensesTypes)"
-                                    :key="expensType.id">
-                                    <td class="text-center">{{ index + 1 }}.</td>
-                                    <!--                                <td>{{ expensType.id }}</td>-->
-                                    <!--                                <td class="text-center">{{ expensType.userID }}.</td>-->
-                                    <td class="text-center">{{ expensType.type }}</td>
-
-                                    <td class="text-center buttons" v-if="userId === expensType.userID">
-                                        <router-link
-                                            :to="{name:'editexpensestypes', params: {id:expensType.id}}"
-                                            class="btn btn-sm btn-warning">
-                                            Upraviť
-                                        </router-link>
-                                        <button class="btn btn-danger btn-sm m-1"
-                                                @click="deleteExpensType(expensType)">
-                                            Odstrániť
-                                        </button>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
+        <div class="container card cardExpensesTypes">
+            <div class="card-body">
+                <div class="d-flex justify-content-between pb-2 mb-2">
+                    <h3 class="card-title"><strong>Typy transakcií</strong>
+                    </h3>
+                    <div>
+                        <button class="btn btn-success buttonNewExpense" type="button"
+                                @click="this.$router.push('/expensestypes/add')">
+                            Vytvoriť nový typ transakcie
+                        </button>
                     </div>
                 </div>
+
+                <table class="table table-hover table-sm table-bordered table-dark">
+                    <thead class="bg-dark text-light">
+                    <tr>
+                        <th width="50" class="text-center">#</th>
+                        <!--                                <th width="100" class="text-center">id</th>-->
+                        <!--                                <th width="100" class="text-center">userId</th>-->
+                        <th class="text-center">Typ transakcie</th>
+                        <th class="text-center" width="200">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class=""
+                        v-for="(expensType, index) in filteredAndSortedExpensesTypes(expensesTypes)"
+                        :key="expensType.id">
+                        <td class="text-center">{{ index + 1 }}.</td>
+                        <!--                                <td>{{ expensType.id }}</td>-->
+                        <!--                                <td class="text-center">{{ expensType.userID }}.</td>-->
+                        <td class="text-center">{{ expensType.type }}</td>
+
+                        <td class="text-center buttons" v-if="userId === expensType.userID">
+                            <router-link
+                                :to="{name:'editexpensestypes', params: {id:expensType.id}}"
+                                class="btn btn-sm btn-warning">
+                                Upraviť
+                            </router-link>
+                            <button class="btn btn-danger btn-sm m-1"
+                                    @click="deleteExpensType(expensType)">
+                                Odstrániť
+                            </button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -187,10 +181,44 @@ export default {
     max-height: 10px;
 }
 
-.cardExpensesTypes {
-    margin-top: 30px;
-    margin-bottom: 30px;
-    border-radius: 18px;
+@media screen and (min-width: 1630px) {
+    .cardExpensesTypes {
+        max-width: var(--max-width-1);
+        margin-top: 10px;
+        border-radius: 18px;
+    }
+}
+
+@media screen and (max-width: 1630px) {
+    .cardExpensesTypes {
+        max-width: var(--max-width-2);
+        margin-top: 10px;
+        border-radius: 18px;
+    }
+}
+
+@media screen and (max-width: 1530px) {
+    .cardExpensesTypes {
+        max-width: var(--max-width-3);
+        margin-top: 10px;
+        border-radius: 18px;
+    }
+}
+
+@media screen and (max-width: 1430px) {
+    .cardExpensesTypes {
+        max-width: var(--max-width-4);
+        margin-top: 10px;
+        border-radius: 18px;
+    }
+}
+
+@media screen and (max-width: 1270px) {
+    .cardExpensesTypes {
+        max-width: var(--max-width-5);
+        margin-top: 10px;
+        border-radius: 18px;
+    }
 }
 
 .buttonNewExpense {
