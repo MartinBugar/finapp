d4aa54666ab7718ccb0051dd76394c809f3d153d
martyx <martin1.bugar@gmail.com>
2023-05-24 08:05:20 +0200

    calendar of names  * added :  - januar  - febraur  - marec  - april

--- resources/js/components/CalendarNames.js
+++ resources/js/components/CalendarNames.js
@@ -1,40 +1,28 @@
 export default [
     {
+        id: 1,
+        name: 'Január',
+        names: ['', 'Alexandra, Karina', 'Daniela', 'Drahoslav', 'Andrea', 'Antónia', 'Bohuslava', 'Severín', 'Alexej', 'Dáša', 'Malvína', 'Ernest', 'Rastislav', 'Radovan', 'Dobroslav', 'Kristína', 'Nataša', 'Bohdana', 'Drahomíra, Mário', 'Dalibor', 'Vincent', 'Zora', 'Miloš', 'Timotej', 'Gejza', 'Tamara', 'Bohuš', 'Alfonz', 'Gašpar ', 'Ema', 'Emil']
+    },
+    {
+        id: 2,
+        name: 'Február',
+        names: ['Tatiana', 'Erika, Erik', 'Blažej', 'Veronika', 'Agáta', 'Dorota', 'Vanda', 'Zoja', 'Zdenko', 'Gabriela', 'Dezider', 'Perla', 'Arpád', 'Valentín', 'Pravoslav', 'Ida, Liana', 'Miloslava', 'Jaromír', 'Vlasta', 'Lívia', 'Eleonóra', 'Etela', 'Roman, Romana', 'Matej', 'Frederik, Frederika', 'Viktor', 'Alexander', 'Zlatica']
+    },
+    {
+        id: 3,
+        name: 'Marec',
+        names: ['Albín', 'Anežka, Bohumil, Bohumila', 'Kazimír', 'Fridrich', 'Radoslav, Radoslava', 'Tomáš', 'Alan, Alana', 'Františka', 'Bruno, Branislav', 'Angela, Angelika', 'Gregor', 'Vlastimil', 'Matilda', 'Boleslav', 'Ľubica', 'Eduard', 'Jozef', 'Víťazoslav, Klaudius', 'Blahoslav', 'Beňadik', 'Adrián', 'Gabriel', 'Marián', 'Emanuel', 'Alena', 'Soňa', 'Miroslav', 'Vieroslava', 'Benjamín']
+    },
+    {
+        id: 4,
+        name: 'Apríl',
+        names: ['Hugo', 'Zita', 'Richard', 'Izidor', 'Miroslava', 'Irena', 'Zoltán', 'Albert', 'Milena', 'Igor', 'Július', 'Estera', 'Aleš', 'Justina', 'Fedor', 'Dana, Danica', 'Rudolf, Rudolfa', 'Valér', 'Jela', 'Marcel', 'Ervín', 'Slavomír', 'Vojtech', 'Juraj', 'Marek', 'Jaroslava', 'Jaroslav', 'Jarmila', 'Lea', 'Anastázia']
+    },
+    {
         id: 5,
         name: 'Máj',
-        names: [
-            null,
-            'Zigmund',
-            'Timea',
-            'Florian',
-            'Lesana',
-            'Hermina',
-            'Moinika',
-            'Ingrida',
-            'Roland',
-            'Viktoria',
-            'Blazena',
-            'Pankrac',
-            'Servac',
-            'Bonifac',
-            'Zofia',
-            'Svetozar',
-            'Gizela',
-            'Viola',
-            'Gertruda',
-            'Bernard',
-            'Zina',
-            'Julia',
-            'Zelmira',
-            'Ela',
-            'Urban',
-            'Dusan',
-            'Iveta',
-            'Viliam',
-            'Vilma',
-            'Ferdinad',
-            'Petronela'
-        ]
+        names: ['', 'Žigmund', 'Timea', 'Florián', 'Lesana', 'Hermína', 'Monika', 'Ingrida', 'Roland', 'Viktória', 'Blažena', 'Pankrác', 'Servác', 'Bonifác', 'Žofia', 'Svetozár', 'Gizela', 'Viola', 'Gertrúda', 'Bernard', 'Zina', 'Júlia', 'Želmira', 'Ela', 'Urban', 'Dušan', 'Iveta', 'Viliam', 'Vilma', 'Ferdinand', 'Petronela']
     },
 
 
